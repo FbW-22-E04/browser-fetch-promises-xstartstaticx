021466c542ba8066355eb0e19b1c5ef03dce9d3c
xstartstaticx <daniel.reiller@icloud.com>
2022-09-28 14:52:39 +0200

    finished exercise

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,20 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
+import "../styles/main.scss";
 // Import any additional modules you want to include below \/
-
 
 // \/ All of your javascript should go here \/
 
+const modal = document.querySelector(".modal");
+
+const sixtySecondPromise = new Promise(() => {
+  setTimeout(() => {
+    modal.style.display = "block";
+  }, 60000);
+});
+
+const close = document.querySelector(".close");
+const closeWindow = () => {
+  modal.style.display = "none";
+};
+
+close.addEventListener("click", closeWindow);
